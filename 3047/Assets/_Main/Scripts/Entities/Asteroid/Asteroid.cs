4bb08384911576abe6f00cd309cc702c0f7daa27
Rucas02 <campolucas2001@gmail.com>
2022-05-27 11:36:17 -0300

    I made a Asteroid Spawner
    It's not finished, right now it's a mess,I have to implement factory.
    
    This spawner should be able to:
    spawn the asteroids in a random point on the right side of the screen
    it has to be able to increment the speed it spawns the asteroids and the amount of them over time.
    And either asign the random values of the asteroids or instead of the spawn asigning those values, it is the asteroid that asign those values to itself.
    if it spawns more than one asteroid it needs to check if the asteroids didn't spawned in the same position.

--- 3047/Assets/_Main/Scripts/Entities/Asteroid/Asteroid.cs
+++ 3047/Assets/_Main/Scripts/Entities/Asteroid/Asteroid.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -8,7 +9,7 @@ public class Asteroid : Ship
 {
     //Now everything is in the same script, that is to make sure everithing works, later I'm going to implement more design patterns
     private Rigidbody _rigidbody;
-    private float _size = 1f;
+    private float _size = 1f;    
     //The stats in Entity.Data should be multiplied or divided by this number, speed, damage, life, etc.
     //The spawner is going to be the one to asign this number as random between minSize and maxSize.
     //The random values should also be asigned in the spawner
@@ -17,15 +18,15 @@ public class Asteroid : Ship
 
     [Header("Random values")] 
     [SerializeField] 
-    private float minSize = .5f;
+    private float _minSize = .5f;
     [SerializeField] 
-    private float maxSize = 2f;
+    private float _maxSize = 2f;
     [SerializeField] [Range(0, 360)]
-    private float angleX = 360;
+    private float _angleX = 360;
     [SerializeField] [Range(0, 360)]
-    private float angleY = 360;
+    private float _angleY = 360;
     [SerializeField] [Range(0, 360)]
-    private float angleZ = 360;
+    private float _angleZ = 360;
     
     protected override void Awake()
     {
@@ -33,9 +34,14 @@ public class Asteroid : Ship
         _rigidbody = GetComponent<Rigidbody>();
     }
 
+    public void InitStats()
+    {
+        
+    }
+    
     protected void Start()
     {
-        transform.eulerAngles = new Vector3(Random.value * angleX, Random.value * angleY, Random.value * angleZ);
+        transform.eulerAngles = new Vector3(Random.value * _angleX, Random.value * _angleY, Random.value * _angleZ);
         transform.localScale = Vector3.one * _size;
     }
 
--- 3047/Assets/_Main/Scripts/Entities/Asteroid/AsteroidSpawner.cs
+++ 3047/Assets/_Main/Scripts/Entities/Asteroid/AsteroidSpawner.cs
@@ -1,8 +1,62 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
 
 public class AsteroidSpawner : MonoBehaviour
 {
+    [Header("Components")]
+    [SerializeField] private Asteroid _asteroid;
     
+    [Header("Info")]
+    [SerializeField] private int _spawnAmount = 1;
+    [SerializeField] private float _trajectoryVariance = 15f;
+    private float _spawnRange;
+    
+    [Header("Rate")]
+    [SerializeField] private float _spawnRate = 2f;
+    private float _lastSpawnedTime;
+    private void Start()
+    {
+        //InvokeRepeating(nameof(Spawn), _spawnRate, _spawnRate);
+        _spawnRange = (1 / (Camera.main.WorldToViewportPoint(new Vector3(1, 1, 0)).y - .5f)) / 2;
+    }
+
+    private void Update()
+    {
+        //My idea was to use the InvokeRepeating, but I dont know how to make it so it calls Spawn(_spawnAmount)
+        SpawnRepeating(_spawnRate, _spawnAmount);
+    }
+
+    private void Spawn()
+    {
+        Vector3 spawnPoint = new Vector3(transform.position.x, Random.Range(-_spawnRange, _spawnRange), 0f);
+
+        float variance = Random.Range(-_trajectoryVariance, _trajectoryVariance);
+        Quaternion rotation = Quaternion.AngleAxis(variance, Vector3.forward);
+        
+        Asteroid asteroid = Instantiate(_asteroid, spawnPoint, rotation);
+    }
+
+    private void Spawn(int spawnAmount)
+    {
+        for (int i = 0; i < spawnAmount; i++)
+        {
+            Spawn();
+        }
+    }
+    private void SpawnRepeating(float repeatRate)
+    {
+        if (!(_lastSpawnedTime + repeatRate < Time.time)) return;
+        _lastSpawnedTime = Time.time;
+        Spawn();
+    }
+    private void SpawnRepeating(float repeatRate, int spawnAmount)
+    {
+        if (!(_lastSpawnedTime + repeatRate < Time.time)) return;
+        _lastSpawnedTime = Time.time;
+        Spawn(spawnAmount);
+    }
 }
