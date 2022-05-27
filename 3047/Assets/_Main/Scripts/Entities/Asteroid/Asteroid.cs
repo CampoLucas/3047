02e918886dce7c544e19c5610f0e2e98f91ca139
Rucas02 <campolucas2001@gmail.com>
2022-05-27 10:26:35 -0300

    A few attributes to randomize the asteroids

--- 3047/Assets/_Main/Scripts/Entities/Asteroid/Asteroid.cs
+++ 3047/Assets/_Main/Scripts/Entities/Asteroid/Asteroid.cs
@@ -1,13 +1,44 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Asteroid : Ship
 {
+    //Now everything is in the same script, that is to make sure everithing works, later I'm going to implement more design patterns
+    private Rigidbody _rigidbody;
+    private float _size = 1f;
+    //The stats in Entity.Data should be multiplied or divided by this number, speed, damage, life, etc.
+    //The spawner is going to be the one to asign this number as random between minSize and maxSize.
+    //The random values should also be asigned in the spawner
+    //It needs the randomization for it to feel natural
+
+
+    [Header("Random values")] 
+    [SerializeField] 
+    private float minSize = .5f;
+    [SerializeField] 
+    private float maxSize = 2f;
+    [SerializeField] [Range(0, 360)]
+    private float angleX = 360;
+    [SerializeField] [Range(0, 360)]
+    private float angleY = 360;
+    [SerializeField] [Range(0, 360)]
+    private float angleZ = 360;
+    
     protected override void Awake()
     {
         base.Awake();
+        _rigidbody = GetComponent<Rigidbody>();
     }
+
+    protected void Start()
+    {
+        transform.eulerAngles = new Vector3(Random.value * angleX, Random.value * angleY, Random.value * angleZ);
+        transform.localScale = Vector3.one * _size;
+    }
+
     public override void OnDieListener()
     {
         base.OnDieListener();
