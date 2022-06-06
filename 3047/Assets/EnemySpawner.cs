0a6ae9e1fc229f6dff537a88c2fcae1bfa772f48
Julian Lanty <julianemanueltejo@gmail.com>
2022-06-06 02:18:05 -0300

    EnemySpawner.cs

--- 3047/Assets/EnemySpawner.cs
+++ 3047/Assets/EnemySpawner.cs
@@ -1,18 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
-public class EnemySpawner : MonoBehaviour
+public class EnemySpawner : MonoBehaviour//TODO finish this
 {
-    // Start is called before the first frame update
-    void Start()
+    public List<GameObject> enemyGroups;
+    public float[] timesToSpawn;
+    private int i = 0;
+    private void Awake()
     {
-        
+         i = 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         
+        if (GameManager.instance.currentGameTime >= timesToSpawn[i])
+        {
+            SpawnGroup(i);
+            i++;
+        }
     }
+
+    private void SpawnGroup(int index)
+    {
+        if (index>=enemyGroups.Count) return;
+        Instantiate(enemyGroups[index]);
+    }
+
 }
