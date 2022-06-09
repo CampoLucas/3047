5e5deb00ef1c2934b4abe750ecf7c8b026d7865a
Julian Lanty <julianemanueltejo@gmail.com>
2022-06-09 03:48:54 -0300

    display de current time en HUD canvas con TMPro

--- 3047/Assets/_Main/Scripts/UI/HUD.cs
+++ 3047/Assets/_Main/Scripts/UI/HUD.cs
@@ -3,16 +3,23 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class HUD : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _scoreText;
     [SerializeField] private TextMeshProUGUI _multiplier;
+    [SerializeField] private TextMeshProUGUI _gameTime;
 
     private void Start()
     {
         GameManager.instance.OnScoreChange += OnScoreChangeListener;
         GameManager.instance.OnMultiplierChange += OnMultiplerChangeListener;
+    }
+
+    private void Update()
+    {
+        _gameTime.text = "Time: " + (int)GameManager.instance.currentGameTime;
     }
 
     void OnScoreChangeListener(float score)
