cb0116aee2b9987b98d506dd89859ff690dda5ef
Julian Lanty <julianemanueltejo@gmail.com>
2022-06-09 03:20:43 -0300

    comentarios en como funciona EnemySpawner.cs

--- 3047/Assets/_Main/Scripts/Entities/Enemy/EnemySpawner.cs
+++ 3047/Assets/_Main/Scripts/Entities/Enemy/EnemySpawner.cs
@@ -4,9 +4,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.PlayerLoop;
 
-public class EnemySpawner : MonoBehaviour//TODO finish this
+public class EnemySpawner : MonoBehaviour
 {
-    [Header("Enemy Groups prefabs")]
+    //This script spawns a gameobject at a specific time ---- 
+    //enemygroups[0] will spawn at the time: timesToSpawn[0]
+    //eg. enemygroup[0] => prefab TargetShotStraight will spawn after timesToSpawn[0]=>30f which is 30seconds
+    //in the inspector Element0 of enemyGroups will spawn after Element0 of timesToSpawn in seconds
+    //elements must be in order to work properly and be legible
     public List<GameObject> enemyGroups;
     public float[] timesToSpawn;
     private int i = 0;
