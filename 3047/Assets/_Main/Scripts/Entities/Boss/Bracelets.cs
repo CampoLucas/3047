987e211698389a97dd141033d8416e2a6dff5cb7
Lu <102192546+Rucas02@users.noreply.github.com>
2022-06-22 11:06:23 -0300

    Now the bracelets regenerate
    I don't know if I like it very much.
    I will make 3 build with only the boss.
    1. How it is right now, only regenerating the bracelets.
    2. Without regenerating, but increasing the speed at which they fire.
    3. Both combined

--- 3047/Assets/_Main/Scripts/Entities/Boss/Bracelets.cs
+++ 3047/Assets/_Main/Scripts/Entities/Boss/Bracelets.cs
@@ -6,11 +6,15 @@ public class Bracelets : Ship
 {
     private Boss _boss;
     private Animator _anim;
+    private SingleStraightShotGun _gun;
+    
+    
 
     protected override void Awake()
     {
         _boss = GetComponentInParent<Boss>();
         _anim = GetComponent<Animator>();
+        _gun = GetComponent<SingleStraightShotGun>();
     }
 
     public override void OnDieListener()
@@ -18,14 +22,36 @@ public class Bracelets : Ship
         _boss.TakeDamage(_boss.Data.Life / 4);
         _boss.IncrementRotationSpeed(20);
         _boss.ChangeRotationDirection();
+        DeactivateBracelet();
         
-        
-        base.OnDieListener();
+        //base.OnDieListener();
     }
 
     public override void TakeDamage(int damage)
     {
         base.TakeDamage(damage);
         _anim.SetTrigger("Damage");
+    }
+
+    public void DeactivateBracelet()
+    {
+        if(_gun)
+            _gun.enabled = false;
+        if(_boss.state != BossState.Dead)
+            StartCoroutine(ToggleBracelet(Data.Speed));
+    }
+
+    public void ActivateBracelet()
+    {
+        _gun.enabled = true;
+    }
+    
+    IEnumerator ToggleBracelet(float time)
+    {
+        _anim.SetBool("Active", false);
+
+        yield return new WaitForSeconds(time);
+
+        _anim.SetBool("Active", true);
     }
 }
