1d5cd298cf73e36c84802a0cd7abb0f370f515c6
Lu <102192546+Rucas02@users.noreply.github.com>
2022-06-28 14:19:13 -0300

    Big error when changing scenes
    I think it is the game manager

--- 3047/Assets/GameOverScreen.cs
+++ 3047/Assets/GameOverScreen.cs
@@ -3,14 +3,44 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.SceneManagement;
 
 public class GameOverScreen : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI _scoreText;
+    [SerializeField] private TMP_Text _scoreText;
+    [SerializeField] private TMP_Text _firstButtonText;
+    [SerializeField] private bool _isWinner;
+
+    private int _levelIndex;
 
     private void Start()
     {
         _scoreText.text = "Score: " + GameManager.instance.currentScore;
+        _levelIndex = SceneManager.GetActiveScene().buildIndex;
+        if (_isWinner && _levelIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            _firstButtonText.text = "CONTINUE";
+        }
+        else if (_isWinner && _levelIndex >= SceneManager.sceneCountInBuildSettings || !_isWinner)
+        {
+            _firstButtonText.text = "RESTART";
+        }
+    }
+
+    public void FirstButton()
+    {
+        if (_isWinner && _levelIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(_levelIndex + 1);
+        }
+        else
+        {
+            SceneManager.LoadScene(_levelIndex);
+        }
+    }
+
+    public void MenuButton()
+    {
+        SceneManager.LoadScene(0);
     }
 }
