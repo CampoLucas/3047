482c23a9b60c7edef8c983d30ab95696b117f081
Julian Lanty <julianemanueltejo@gmail.com>
2022-06-29 17:16:06 -0300

    boss 2 take damage animation

--- 3047/Assets/_Main/Scripts/Entities/Boss 2/Wall.cs
+++ 3047/Assets/_Main/Scripts/Entities/Boss 2/Wall.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class Wall : Ship
 {
+    private Animator _anim;
+
+    protected override void Awake()
+    {
+        _anim = GetComponent<Animator>();
+    }
+    public override void TakeDamage(int damage)
+    {
+        base.TakeDamage(damage);
+        _anim.SetTrigger("Damage");
+    }
+
     public override void OnDieListener()
     {
         base.OnDieListener();
