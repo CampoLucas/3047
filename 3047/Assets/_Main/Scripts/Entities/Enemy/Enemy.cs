9570b42c6bfbd69531666c70b3dd3542b93f5b64
Julian Lanty <julianemanueltejo@gmail.com>
2022-06-29 16:47:47 -0300

    enemies deactivate when passing left screen bound

--- 3047/Assets/_Main/Scripts/Entities/Enemy/Enemy.cs
+++ 3047/Assets/_Main/Scripts/Entities/Enemy/Enemy.cs
@@ -12,15 +12,25 @@ public class Enemy : Ship
     [SerializeField] private float points = 2f;
     [SerializeField] private float addedMultiplier = 0.02f;
     
+    Camera cam;
+    Vector2 screenSize;
+    
     protected override void Awake()
     {
         base.Awake();
         //if(_damagable)
         //    _damagable.OnDie.AddListener(OnDieListener);
-        
-        
+        cam = Camera.main;
+        GetScreenSize();
     }
+    private void GetScreenSize() => screenSize = new Vector2((1 / (cam.WorldToViewportPoint(new Vector3(1, 1, 0)).x - .5f)) / 2, (1 / (cam.WorldToViewportPoint(new Vector3(1, 1, 0)).y - .5f)) / 2);
 
+    private void Update()
+    {
+        //cuando se salen del borde izquierdo de la pantalla se llama al ondie
+        Vector3 pos = transform.position;
+        if (pos.x <= -screenSize.x) _damageable.OnDie.Invoke();
+    }
 
     public override void OnDieListener()
     {
